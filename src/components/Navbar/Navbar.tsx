bbee0cfb57e3ac5e6cb27c833186cc3a143a69b2
Ramzy Ahmed <ramzyahamed96@gmail.com>
2024-02-23 04:40:15 +0530

    web menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -12,8 +12,10 @@ import MenuItem from '../MenuItem/MenuItem';
 import Button from '../Button/Button';
 import { usePathname } from 'next/navigation';
 import WeatherWidget from '../Weather/WeatherWidget';
+import WebMenuSection from './WebMenuSection/WebMenuSection';
 
 const Navbar = () => {
+    const [hoveredId, setHoveredId] = useState(0);
     const [openMenu, setOpenMenu] = useState(false);
     const [openItem, setOpenItem] = useState(null);
     const pathname = usePathname();
@@ -23,6 +25,12 @@ const Navbar = () => {
     const openItemMenu = (title: any) => {
         setOpenItem((prevItem: any) => (prevItem === title ? null : title));
     };
+
+    const handleHover = (id: number) => {
+        console.log(id)
+        setHoveredId(id)
+    }
+
     return (
         <>
             <nav className='navbar'>
@@ -40,17 +48,38 @@ const Navbar = () => {
                 </div>
                 {/* web */}
                 <ul className='navMenu'>
-                    {menuItems.map((item, index) => {
+                    {menuItems.map((item: any, index) => {
+                        // const filteredItem = menuItems.find((x: any) => x.id === hoveredId);
                         return (
-                            <li key={index}>
-                                <Link href={item.url}>
+                            <li key={index} className='item navList' onMouseEnter={() => handleHover(item.id)} >
+                                <Link href={item.url} className=''>
                                     <div className={pathname === item.url ? 'activeWebLink' : 'webNavLink'}>
                                         <span>
                                             {item.title}
                                         </span>
                                     </div>
-
                                 </Link>
+                                <div className="arrow" />
+                                <div className="submenu-wrapper">
+                                    <div className="item-submenu">
+                                        {/* <div className="submenu-group">
+                                            <a href="#" className="submenu-link-master">Heladeras y freezers</a>
+                                            <a href="#" className="submenu-link">Heladeras</a>
+                                            <a href="#" className="submenu-link">Freezers</a>
+                                            <a href="#" className="submenu-link">Cavas y Frigobares</a>
+                                            <WebMenuSection />
+                                        </div> */}
+                                        {/* {
+                                            item.menu.map((menuItem: any, menuIndex: number) => {
+                                                return (
+                                                    <WebMenuSection menuItem={menuItem} menuIndex={menuIndex} />
+                                                )
+                                            })
+                                        } */}
+                                        <WebMenuSection menuItems={menuItems} hoveredId={hoveredId} />
+
+                                    </div>
+                                </div>
                             </li>
                         );
                     })}
